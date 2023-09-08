56cea4ba4b4d05f04fe8965419bd0a683023191d
Martin <141005513+martinproject1@users.noreply.github.com>
2023-09-08 08:34:58 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+alert('div text'); {
+    <i class="fas fa-vihara    tashchyanstyle"></i>
+}
+CSSLayerStatementRule(
+    KeyboardEvent(' ' )
+)
+
+for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        alert(div)
+    }
+}
